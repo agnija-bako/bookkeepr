9698191f43d142f71d1c20b6b79379a965dc8f4f
agnija-bako <agnijabako@yahoo.com>
2022-04-15 17:13:10 +0200

    Added Frontend

--- Bookkeepr/Controllers/BooksController.cs
+++ Bookkeepr/Controllers/BooksController.cs
@@ -10,18 +10,19 @@ namespace Bookkeepr.Api.Controllers
         [HttpGet]
         public Book Get()
         {
-            return new Book 
+            return new Book
             {
-                Id = 1, 
-                Author = "Elizabeth Kostova", 
-                Description = "The Historian is athe story of a young woman plunged into a labyrinth where the secrets of her family's past connect to an inconceivable evil", 
-                Genres = new List<Genre>() 
-                { 
-                    new Genre { Id = 1, Name = "Gothic" } 
+                Id = 1,
+                Author = "Elizabeth Kostova",
+                Description =
+                    "The Historian is a story of a young woman plunged into a labyrinth where the secrets of her family's past connect to an inconceivable evil",
+                Genres = new List<Genre>
+                {
+                    new() {Id = 1, Name = "Gothic"}
                 },
-                Publisher = "Back Bay Books", Title = "The Historian", 
-                Rating = 3.9, 
-                Year = 2010 
+                Publisher = "Back Bay Books", Title = "The Historian",
+                Rating = 3.9,
+                Year = 2010
             };
         }
     }
